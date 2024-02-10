a1a72ae416ea4e5853e33de75a54c8db79d8a25b
iska9der <iskander94y@gmail.com>
2024-02-11 01:23:06 +0300

    debounce fetch, loading state, disable input on loading

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,33 +1,43 @@
-
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
   name: string;
   label: string;
-  hint?: string | null;
-  inputValue?: string | undefined;
-  onInputChange: (value: string) => void;
+  inputValue?: number | undefined;
+  onInputChange: (value: number) => void;
+  className?: string | undefined;
+  disabled?: boolean
 }
 
 export default function InputField({
   name,
   label,
-  hint,
   inputValue,
   onInputChange,
-  ...props
+  className,
+  disabled = false,
 }: Props) {
   return (
-    <div className={`relative flex flex-col bg-[#F5F8FA] rounded-xl ${props.className}`}>
+    <div
+      className={`relative flex flex-col rounded-xl ${className}`}
+    >
       <label className="flex-1 pb-[8px] text-[20px]" htmlFor={name}>
-          {label}
-        </label>
+        {label}
+      </label>
       <input
-          className="flex-1 h-[48px] text-[32px] bg-[#F5F8FA] w-full outline-none"
-          name={name}
-          type="number"
-          placeholder="0"
-          value={inputValue}
-          onChange={(event) => onInputChange(event.target.value)}
-        />
+        className="flex-1 h-[48px] text-[32px] bg-inherit w-full outline-none"
+        disabled={disabled}
+        name={name}
+        inputMode="numeric"
+        pattern="[0-9]*"
+        type="text"
+        placeholder="0"
+        value={inputValue?.toString()}
+        onChange={(event) => {
+          const value =
+            event.target.value.length == 0 ? 0 : parseFloat(event.target.value);
+
+          return onInputChange(value);
+        }}
+      />
     </div>
   );
 }
--- src/components/SourceAndTarget.tsx
+++ src/components/SourceAndTarget.tsx
@@ -1,15 +1,49 @@
 import React, { useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
 import { useApp } from "../lib/AppContext";
 import InputField from "./InputField";
 import SelectCurrency from "./SelectCurrency";
 
 export default function SourceTarget() {
   const { currencies } = useApp();
-  const [sourceValue, setSourceValue] = useState("");
-  const [targetValue, setTargetValue] = useState("");
+
+  // currency identificators
   const [sourceCurrency, setSourceCurrency] = useState(1);
   const [targetCurrency, setTargetCurrency] = useState(3);
+
+  const [isExchangeLoading, setIsExchangeLoading] = useState(false);
+
   const sourceHint = currencies.find((el) => el.id == sourceCurrency)?.hint;
+
+  const [sourceValue, setSourceValue] = useState<number>(0);
+  const [targetValue, setTargetValue] = useState<number>(0);
+  const debounceExchange = useDebouncedCallback(
+    (currencyId: number, value: number, setValue: (value: number) => void) => {
+      fetchExchangeData(currencyId, value, setValue);
+    },
+    1500
+  );
+
+  const fetchExchangeData = async (
+    currencyId: number,
+    value: number,
+    setValueCallback: (value: number) => void
+  ) => {
+    console.log(value);
+    if (value == 0) {
+      return setValueCallback(0);
+    }
+
+    console.log("fetch api/exchange");
+    setIsExchangeLoading(true);
+    await new Promise((_) => {
+      setTimeout(() => {
+        const outputValue = 1337 + value;
+        setValueCallback(outputValue);
+        setIsExchangeLoading(false);
+      }, 2000);
+    });
+  };
 
   const swap = () => {
     setSourceValue(targetValue);
@@ -20,13 +54,17 @@ export default function SourceTarget() {
 
   return (
     <div className="relative flex flex-col gap-y-6">
-      <Wrapper>
+      <Wrapper disabled={isExchangeLoading}>
         <InputField
           className="flex-1"
+          disabled={isExchangeLoading}
           name="source"
           label="Отдаете"
           inputValue={sourceValue}
-          onInputChange={(value) => setSourceValue(value)}
+          onInputChange={(value) => {
+            setSourceValue(value);
+            debounceExchange(sourceCurrency, value, setTargetValue);
+          }}
           // todo: currencyExcluded: pass targetCurrency
         />
         <div className="flex flex-col items-end gap-y-2">
@@ -36,13 +74,17 @@ export default function SourceTarget() {
           <SelectCurrency value={sourceCurrency} onChange={setSourceCurrency} />
         </div>
       </Wrapper>
-      <Wrapper>
+      <Wrapper disabled={isExchangeLoading}>
         <InputField
           className="flex-1"
+          disabled={isExchangeLoading}
           name="target"
           label="Получаете"
           inputValue={targetValue}
-          onInputChange={(value) => setTargetValue(value)}
+          onInputChange={(value) => {
+            setTargetValue(value);
+            debounceExchange(targetCurrency, value, setSourceValue);
+          }}
         />
         <SelectCurrency value={targetCurrency} onChange={setTargetCurrency} />
       </Wrapper>
@@ -51,9 +93,19 @@ export default function SourceTarget() {
   );
 }
 
-function Wrapper({ children }: { children: React.ReactNode }) {
+function Wrapper({
+  children,
+  disabled = false,
+}: {
+  children: React.ReactNode;
+  disabled?: boolean;
+}) {
   return (
-    <div className="relative flex flex-row items-end bg-[#F5F8FA] rounded-xl px-4 py-5 gap-x-2">
+    <div
+      className={`relative flex flex-row items-end ${
+        disabled ? "bg-[#cdd0d3]" : "bg-[#F5F8FA]"
+      } rounded-xl px-4 py-5 gap-x-2`}
+    >
       {children}
     </div>
   );
@@ -63,7 +115,7 @@ function SwapButton({ onSwap }: { onSwap: () => void }) {
   return (
     <div
       onClick={onSwap}
-      className="rounded-full h-11 w-11 flex bg-black absolute m-auto left-0 right-0 top-0 bottom-0 transition ease-in-out hover:scale-110"
+      className="rounded-full h-[56px] w-[56px] flex bg-[#3E3F40] absolute m-auto left-0 right-0 top-0 bottom-0 transition ease-in-out hover:scale-110"
     >
       <img src="assets/swap.png" className="m-auto" />
     </div>
